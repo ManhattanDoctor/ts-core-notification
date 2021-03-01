b1e8eeb8b52ecef5eab257ac87bfc9c9f659f036
Renat Gubaev <>
2021-03-01 15:03:57 +0300

    auto commit

--- src/dto/INotificationListDto.ts
+++ src/dto/INotificationListDto.ts
@@ -3,5 +3,4 @@ import { IPaginable, IPagination } from '@ts-core/common/dto';
 import { INotification } from '../INotification';
 
 export interface INotificationListDto extends IPaginable<INotification>, ITraceable {}
-
 export interface INotificationListDtoResponse extends IPagination<INotification> {}
