f0d66caa92c802d492ae4f94cb608ead50fbae0b
Renat Gubaev <>
2021-03-03 21:07:38 +0300

    auto commit

--- src/dto/preference/INotifablePreferenceListDto.ts
+++ src/dto/preference/INotifablePreferenceListDto.ts
@@ -2,9 +2,9 @@ import { IFilterable } from '@ts-core/common/dto';
 import { ITraceable } from '@ts-core/common/trace';
 import { INotifablePreference } from '../../INotifablePreference';
 
-export interface INotifablePreferencesListDto extends IFilterable<INotifablePreference>, ITraceable {}
+export interface INotifablePreferenceListDto extends IFilterable<INotifablePreference>, ITraceable {}
 
-export interface INotifablePreferencesListDtoResponse extends Array<INotifablePreferenceItem> {}
+export interface INotifablePreferenceListDtoResponse extends Array<INotifablePreferenceItem> {}
 
 export interface INotifablePreferenceItem extends INotifablePreference {
     channelsAvailable: Array<string>;
