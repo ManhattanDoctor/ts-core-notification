82bb39ec5de8c54024e3cfbfd083b79f527cdb61
Renat Gubaev <>
2021-03-04 17:25:10 +0300

    auto commit

--- src/dto/preference/INotificationPreferenceEditDto.ts
+++ src/dto/preference/INotificationPreferenceEditDto.ts
@@ -1,10 +1,11 @@
 import { ITraceable } from '@ts-core/common/trace';
 import { NotifableUid } from '../../INotifable';
 import { INotificationPreference } from '../../INotificationPreference';
+import { INotificationPreferenceItem } from './INotificationPreferenceListDto';
 
 export interface INotificationPreferenceEditDto extends ITraceable {
     notifableUid: NotifableUid;
-    items: Array<{ type: string; channels: Array<string> }>;
+    items: Array<INotificationPreferenceItem>;
 }
 
 export declare type INotificationPreferenceEditDtoResponse = Array<INotificationPreference>;
--- src/dto/preference/INotificationPreferenceListDto.ts
+++ src/dto/preference/INotificationPreferenceListDto.ts
@@ -1,15 +1,13 @@
 import { IFilterable } from '@ts-core/common/dto';
 import { ITraceable } from '@ts-core/common/trace';
-import { INotificationPreference, NotificationPreference } from '../../INotificationPreference';
+import { INotificationPreference } from '../../INotificationPreference';
 
 export interface INotificationPreferenceListDto extends IFilterable<INotificationPreference>, ITraceable {}
 
 export interface INotificationPreferenceListDtoResponse extends Array<INotificationPreferenceItem> {}
 
-export interface INotificationPreferenceItem<T = string> extends INotificationPreference<T> {
-    channelsAvailable: Array<string>;
-}
-
-export class NotificationPreferenceItem<T = string> extends NotificationPreference<T> implements INotificationPreferenceItem<T> {
+export interface INotificationPreferenceItem<T = string> {
+    type: T;
+    channels: Array<string>;
     channelsAvailable: Array<string>;
 }
