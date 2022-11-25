d1db4a56f881832e83d9fa2aed2f80b981cc1643
Renat Gubaev <renat.gubaev@gmail.com>
2022-11-25 21:16:28 +0300

    no message

---
+++ src/public-api.ts
@@ -0,0 +1,18 @@
+export * from './INotifable';
+export * from './INotification';
+export * from './INotificationResult';
+export * from './INotificationChannel';
+export * from './INotificationMessage';
+export * from './INotificationTemplate';
+export * from './INotificationPreference';
+//
+export * from './dto/INotificationSettingsDto';
+export * from './dto/notification/INotificationListDto';
+export * from './dto/preference/INotificationPreferenceAddDto';
+export * from './dto/preference/INotificationPreferenceEditDto';
+export * from './dto/preference/INotificationPreferenceListDto';
+export * from './dto/template/INotificationTemplateAddDto';
+export * from './dto/template/INotificationTemplateEditDto';
+export * from './dto/template/INotificationTemplateListDto';
+//
+export * from './settings/NotificationSettingsLoader';
